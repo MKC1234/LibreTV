3bfbfb883fdc99b86c1b0e9caefdfb21be70c3b0
MKC1234 <1845320124@qq.com>
2026-08-02 17:13:10 +0800

    Update customer site names in customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,11 +1,11 @@
 const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://bfzyapi.com/api.php/provide/vod',
-        name: '如意资源',
+        name: '暴风资源',
     },
     source2: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
-        name: '不知道',
+        name: '如意资源',
     }
 };
 
